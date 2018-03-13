dc322bf6feacaad0a18ca3718f32dac18667b119
Gustavo Oliveira <gusttavo@live.com>
2018-03-13 15:26:41 -0300

    Ajustes

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,3 +1,18 @@
-$(".skill").click(function(){
+$("#skill").click(function(){
     $(".card").hide();
+    $(".medias-sociais").hide();
+    $(".card-skill").removeClass("hide");
+});
+
+$("#more").click(function(){
+    $(".skills-1").addClass("hide");
+    $(".skills-2").removeClass("hide");
+});
+
+$("#back").click(function(){
+    $(".card-skill").addClass("hide");
+    $(".card").show();
+    $(".medias-sociais").show();
+    $(".skills-1").removeClass("hide");
+    $(".skills-2").addClass("hide");
 });
